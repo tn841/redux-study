dcc7e4fa40e43fbaead65f6bbd2cae7ac16b679c
tn841 <tn841@naver.com>
2020-07-28 14:21:09 +0900

    #1.4 Redux 기초 복습, 코드 개선

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 number.innerText = 0;
 
+const PLUS = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (state=0, action) => {
   // reducer function
   // - modify state
-  
-  if (action.type === "PLUS"){
-    return state + 1;
-  } else if (action.type === 'MINUS') {
-    return state - 1;
+  switch (action.type) {
+    case PLUS:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
   }
-  return state;
 };
 const countStore = createStore(countModifier);  //create store
 
@@ -25,6 +29,6 @@ const onChange = () => {
 }
 countStore.subscribe(onChange)
 
-plus.addEventListener('click', () => countStore.dispatch({type: 'PLUS'}))
-minus.addEventListener('click', () => countStore.dispatch({type: 'MINUS'}))
+plus.addEventListener('click', () => countStore.dispatch({type: PLUS}))
+minus.addEventListener('click', () => countStore.dispatch({type: MINUS}))
 
