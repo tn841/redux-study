64507af8529d4ac56123f651b2a4f10ada52f7fb
tn841 <tn841@naver.com>
2020-07-28 15:21:03 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -9,12 +9,20 @@ const ul = document.querySelector('ul');
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const dispatchAddToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+const dispatchDeleteToDo = ()
+
 const reducer = (state=[], action) => {
   // NEVER NUTATE STATE!!
   console.log(action)
   switch (action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text, id: Date.now()}];
+      return [{text: action.text, id: Date.now()}, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -23,12 +31,22 @@ const reducer = (state=[], action) => {
 }
 const store = createStore(reducer);
 
+const deleteTodo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(dispatchDeleteToDo(id))
+}
+
 store.subscribe( ()=>{
-  console.log('change someting')
   ul.innerText = '';
   store.getState().map( (todo) => {
     let li = document.createElement('li');
+    li.id = todo.id
     li.innerText = todo.text;
+
+    const btn = document.createElement('button');
+    btn.innerText = 'del';
+    btn.addEventListener('click', deleteTodo);
+    li.appendChild(btn)
     ul.appendChild(li);
   })
   
@@ -36,7 +54,7 @@ store.subscribe( ()=>{
 
 const onSubmit = (e) => {
   e.preventDefault();
-  store.dispatch({type: ADD_TODO, text: input.value})
+  store.dispatch(dispatchAddToDo(input.value))
   input.value="";
 }
 
