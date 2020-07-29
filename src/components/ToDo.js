91b20759600018454b1c7ec6796e225f86d05a18
tn841 <tn841@naver.com>
2020-07-29 16:05:19 +0900

    #4.1 createAction

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -7,8 +7,9 @@ function ToDo({text, onBtnClick, id}) {
     return (
         <li>
             <Link to={`/${id}`}>
-            {text} <button onClick={onBtnClick}>Del</button>
+            {text} 
             </Link>
+            <button onClick={onBtnClick}>Del</button>
         </li>
     )
 }
