8701fef87da0d08f5bf3e47048b0605d0bc61753
tn841 <tn841@naver.com>
2020-07-29 09:31:21 +0900

    #3.1 Connection the Store

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import store, { addTodo } from '../store';
 
 function Home(){
     const [text, settext] = useState("") //hooks
@@ -11,6 +12,7 @@ function Home(){
         e.preventDefault();
         console.log(text);
         settext("")
+        store.dispatch(addTodo(text))
     }
 
     return (
---
+++ src/store.js
@@ -0,0 +1,43 @@
+import { createStore } from 'redux';
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addTodo = (text) => {
+    return {
+        type: ADD,
+        text
+    }
+}
+
+export const deleteTodo = (id) => {
+    return {
+        type: DELETE,
+        id
+    }
+}
+
+const reducer = (state=[], action) => {
+    console.log(state, action)
+    switch (action.type) {
+        case ADD:
+            return [ {text: action.text, id: Date.now()}, ...state]
+        case DELETE:
+            return state.filter( todo => todo.id !== action.id );
+        default :
+            return state;
+    }
+}
+const store = createStore(reducer);
+
+store.subscribe( () => {
+    console.log('some thing is changed');
+    // 변화 시켜야하는 elements를 찾아서 
+    // 직접 dom을 수정하는 작업을 하지 않아도 된다.
+    // react-redux가 해준다.
+
+    // react와 redux store를 연결시키는 작업은 
+    // index.js에서 <Provider/>를 통해 이루어진다.
+})
+
+export default store;
