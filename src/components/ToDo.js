c160b99305f7a3ea2b73b9d948292a6cbe7ab5e7
tn841 <tn841@naver.com>
2020-07-29 15:49:27 +0900

    3.5. Detail Page

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-function ToDo({text, onBtnClick}) {
+function ToDo({text, onBtnClick, id}) {
     return (
         <li>
+            <Link to={`/${id}`}>
             {text} <button onClick={onBtnClick}>Del</button>
+            </Link>
         </li>
     )
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function Detail(){
+function Detail({todo}){
+    const id = useParams();
+
     return (
         <div>
-            Detail
+            <h1>{todo?.text}</h1>
+            <h5>Created at : {todo?.id} </h5>
         </div>
     )
 }
 
-export default Detail;
+function mapStateToProps(state, ownProps){
+    const {match: {params: {id}}} = ownProps
+    console.log(id)
+    return {
+        todo: state.find(todo => todo.id === parseInt(id))
+    }
+}
+
+export default connect(mapStateToProps) (Detail);
 
